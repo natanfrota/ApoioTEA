84c816dd882a293108924fb2364881147e24327d
Natan Frota <natanfrota31@gmail.com>
2024-11-17 13:43:07 -0300

    Modulo de inserção na tabela de voluntário

--- ApoioTEA/src/main/java/dao/VoluntarioDAO.java
+++ ApoioTEA/src/main/java/dao/VoluntarioDAO.java
@@ -6,50 +6,76 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.xml.transform.Result;
-
 import modelo.Voluntario;
 
 public class VoluntarioDAO {
-	private List<Voluntario> voluntarios; 
-	
+	private List<Voluntario> voluntarios;
+
 	public void inserirVoluntario(Voluntario voluntario) {
 		String insercaoUsuario = "insert into usuario (nome, email, senha, cidade, "
-				  + "estado, tipo) values (?,?,?,?,?,?)";
-		String insercaoVoluntario = "insert into voluntario (experiencia, habilidades)"
-				+ " values (?, ?)";
-		
-		try (Connection conexao = Conexao.criarConexao()){
+				+ "estado, tipo) values (?,?,?,?,?,?)";
+		String insercaoVoluntario = "insert into voluntario (usuario_id, experiencia, "
+				+ "habilidades) values (?, ?, ?)";
+
+		Connection conexao = null;
+		PreparedStatement ps = null, ps2 = null;
+		ResultSet rs = null;
+
+		try {
+			conexao = Conexao.criarConexao();
 			conexao.setAutoCommit(false);
-			
-			try (PreparedStatement ps = conexao.prepareStatement(insercaoUsuario, 
-					PreparedStatement.RETURN_GENERATED_KEYS)) {
-				ps.setString(1, voluntario.getNome());
-				ps.setString(2, voluntario.getEmail());
-				ps.setString(3, voluntario.getSenha());
-				ps.setString(4, voluntario.getCidade());
-				ps.setString(5, voluntario.getEstado());
-				ps.setString(6, "voluntário");
-				
-				ps.executeUpdate();
-				
-				ResultSet rs = ps.getGeneratedKeys();
-				int idUsuario;
-				if(rs.next())
-					idUsuario = rs.getInt("id");
-				
-				
-				
-			} catch (SQLException e) {
-				System.err.println(e);
+
+			ps = conexao.prepareStatement(insercaoUsuario,
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, voluntario.getNome());
+			ps.setString(2, voluntario.getEmail());
+			ps.setString(3, voluntario.getSenha());
+			ps.setString(4, voluntario.getCidade());
+			ps.setString(5, voluntario.getEstado());
+			ps.setString(6, "voluntário");
+			System.out.println("Dados inseridos na tabelas usuário.");
+
+			ps.executeUpdate();
+
+			rs = ps.getGeneratedKeys();
+			rs.next();
+			int idUsuario = rs.getInt(1);
+
+			ps2 = conexao.prepareStatement(insercaoVoluntario);
+			ps2.setInt(1, idUsuario);
+			ps2.setString(2, voluntario.getExperiencia());
+			ps2.setString(3, voluntario.getHabilidades());
+			ps2.executeUpdate();
+			System.out.println("Dados inseridos na tabelas voluntário.");
+
+			conexao.commit();
+			conexao.setAutoCommit(true);
+
+		} catch (SQLException e) {
+			if (conexao != null) {
+				try {
+					conexao.rollback();
+				} catch (SQLException e2) {
+					System.err.println(e2);
+				}
 			}
-			
-		} catch (Exception e) {
-			
+		} finally {
+			try {
+				if(rs != null)
+					rs.close();
+				if(ps2 != null)
+					ps2.close();
+				if(ps != null)
+					ps.close();
+				if (conexao != null)
+					conexao.close();			
+			} catch (SQLException e2) {
+				System.err.println(e2);
+			}
 		}
 	}
-	
-	public List<Voluntario> selecionarVoluntarios(){
-		
+
+	public List<Voluntario> selecionarVoluntarios() {
+		return voluntarios;
 	}
 }
